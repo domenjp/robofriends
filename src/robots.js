90699676600e30915e7beed6dbdc24a908bd7928
Juan Pablo Domenech <jpdomenech@megeve.cl>
2020-07-20 11:57:56 -0400

    first release commit

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+        id:1,
+        name: 'Leanne Graham',
+        username: 'Breat',
+        email:'Sincere@april.biz'
+    },
+    {
+        id:2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email:'Snana@melisa.tv'
+    },
+    {
+        id:3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email:'Nathan@yesenia.net'
+    },
+    {
+        id:4,
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email:'Julianne.OConner@kory.org'
+    },
+    {
+        id:5,
+        name: 'Chelsey Dietrich',
+        username: 'KAmren',
+        email:'Sincere2@april.biz'
+    },
+    {
+        id:6,
+        name: 'Leanne Graham',
+        username: 'Breat',
+        email:'Sincere@april.biz'
+    },
+    {
+        id:7,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email:'Snana@melisa.tv'
+    },
+    {
+        id:8,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email:'Nathan@yesenia.net'
+    },
+    {
+        id:9,
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email:'Julianne.OConner@kory.org'
+    },
+    {
+        id:10,
+        name: 'Chelsey Dietrich',
+        username: 'KAmren',
+        email:'Sincere2@april.biz'
+    },
+
+]
